eafce9de4c8a1bba2636a6567903e30d6633b0fb
Soumye <soumye@iitk.ac.in>
2018-09-19 19:08:39 +0530

    # of states to backtrack from =1

--- arguments_mujoco.py
+++ arguments_mujoco.py
@@ -33,7 +33,7 @@ def achieve_arguments():
     # parse.add_argument('--no-sil', action='store_true', help='check if use the sil')
     parse.add_argument('--k-states', type=int, default=64, help='Number of top value states to train Backtracking Model on')
     parse.add_argument('--model-type', type=str, default='bw', help='Which model to use: "vanilla", "sil", "bw"' )
-    parse.add_argument('--num-states', type=int, default=5, help='Number of high value state to actually backtrack on')
+    parse.add_argument('--num-states', type=int, default=1, help='Number of high value state to actually backtrack on')
     # parse.add_argument('--num-traces', type=int, default=1, help='Number of traces for each high value state ')
     parse.add_argument('--trace-size', type=int, default=20, help='Number of steps to backtrack on for a given high value state ie length of trajectory')
     parse.add_argument('--state-coef', type=float, default=1e-7, help='regularization for state generator coefficient')
